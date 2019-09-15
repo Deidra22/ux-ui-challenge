9761aa1ab85a5128d83b74ef3d9d78f0e8769ac7
Deidra22 <Deidra22@users.noreply.github.com>
2019-09-15 15:03:22 -0700

    still struggling with concept of examples for some reason. Design is coming together. have scroll down to contact form

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,16 @@ function scrollDown(element) {
   
   const btns = document.querySelectorAll('.js-btn');
   const sections = document.querySelectorAll('.js-section');
+
+  const conbtn = document.querySelectorAll('.contact-link');
+  const form = document.querySelectorAll('.contact-form');
   
   btns[0].addEventListener('click', () => {
     scrollDown(sections[0]);
+});
+
+    conbtn[0].addEventListener('click', () => {
+        scrollDown(form[0]);
   });
 
 // Slide In 
