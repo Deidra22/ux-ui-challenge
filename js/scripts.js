221a6c03a6152daed298f8c93ac567ee2172d9bf
Deidra22 <Deidra22@users.noreply.github.com>
2019-09-15 21:12:13 -0700

    clean up

--- js/scripts.js
+++ js/scripts.js
@@ -22,32 +22,4 @@ function scrollDown(element) {
         scrollDown(form[0]);
   });
 
-// Slide In 
-// function debounce(func, wait = 20, immediate = true){
-//     let timeout;
-//     return function(){
-//         const context = this, args = arguments;
-//         const later = function(){
-//             timeout = null;
-//             if(!immediate) func.apply(context.args);
-//         };
-//         const callNow = immediate && !timeout;
-//         clearTimeout(timeout);
-//         timeout = setTimeout(later, wait);
-//         if(callNow) func.apply(context.args);
-//     };
-// };
-
-
-//   const sliderDivs = document.querySelectorAll('.slide-in');
-//   function checkSlide(e){
-//     console.log(window.scrollY);
-//       sliderDivs.forEach(slideDiv =>{
-//         const slideInAt = (window.scrollY + window.innerHeight) - slideDiv.height / 2;
-//         console.log(slideInAt);
-//       });
-//   }
-
-//   window.addEventListener('scroll', debounce(checkSlide));
-
  
